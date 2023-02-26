1a1b331548e3600845872096e6eb8a80262e0075
John Pow <johnowed@gmail.com>
2023-02-26 14:40:33 -0800

    got 15 second penality to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,41 +17,41 @@ startbutEl.setAttribute('style', "display:inline");
 // answersEl.setAttribute('style', "display:inline");
 // responseEl.setAttribute('style', "display:inline");
 
+
+let round = 0;
+
 startbutEl.addEventListener("click", function () {
     startEl.setAttribute('style', "display:none");
     startbutEl.setAttribute('style', "display:none");
-    countdown();    
-    
+    countdown();
+    askQuestion(round);
+
 });
+
 
 const myQuestions = [
     {
         question: 'What color is the sky generally?',
         choices: ['blue', 'black', 'green', 'red orange'],
+        correctAnswer: 0,
+    },
+    {
+        question: 'What color is the sky on a rainy day?',
+        choices: ['blue', 'black', 'green', 'red orange'],
         correctAnswer: 1,
     },
     {
-        question: 'What color is the sky on a rainy day?',
-        choices: ['blue', 'black', 'green', 'red orange'],
-        correctAnswer: 0,
-    },
-    {
-        question: 'What color is the sky on a rainy day?',
-        choices: ['blue', 'black', 'green', 'red orange'],
-        correctAnswer: 0,
+        question: 'What color is the sky at night when sailors delight?',
+        choices: ['blue', 'red orange','black', 'green' ],
+        correctAnswer: 1,
     },
 ]
 
 
-
-
-let round = 0;
-let penalty = 0;
-let solution;
 // const buttons = [buttaEl,buttbEl,buttcEl,buttdEl]
-function askQuestion(a,b) {
-    
-    console.log('b is in aksqus' + b)
+const askQuestion = function(i) {
+
+if (i<myQuestions.length) {
     questionEl.setAttribute('style', "display:inline");
     answersEl.setAttribute('style', "display:inline");
 
@@ -65,43 +65,37 @@ function askQuestion(a,b) {
     buttaEl.addEventListener("click", function () {
         if (solution === 0) {
             console.log('hurray0');
-            round = a + 1;
-            // return askQuestion();
+            return askQuestion(i+1);
         } else {
             console.log('boo0');
-            round = a + 1;
-            penalty = b + 1;
-            console.log('first '+ penalty);
+            timeLeft -= 15;
+            return askQuestion(i+1);
         }
         });
-        
-        
+             
     buttbEl.addEventListener("click", function () {
         if (solution === 1) {
             console.log('hurray1');
-            round = a + 1;
+            return askQuestion(i+1);
 
         } else {
             console.log('boo1');
-            round = a + 1;
-            penalty = b + 1;
+            timeLeft -= 15;
+            return askQuestion(i+1);
         }
-        });
-
-        
-
-console.log('second '+penalty);
-return [round, penalty];
-
+        })
+    } else {
+        startEl.textContent = 'Game Over!'
+        startEl.setAttribute('style', "display:inline");
+        questionEl.setAttribute('style', "display:none");
+        answersEl.setAttribute('style', "display:none");
+        clearInterval(timeInterval);
+    return;
+    }
 };
-
-
 
 let timeLeft = 60;
 let timeInterval;
-let i = 0;
-let j = 0;
-let newVar = [];
 function countdown() {
     
     // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
@@ -109,28 +103,15 @@ function countdown() {
         
         timeLeft--;
         timerEl.textContent = `Timer: ${timeLeft}`;
-        console.log('what is j ' +j)
-        newVar = askQuestion(i,j);
-        i = newVar[0];
-        j = newVar[1];
-        console.log('i is here 1:' + i)
-        console.log('j is here 1:' + j)
-        console.log('newvar is here 1:' + newVar[1])
-        // ability to penalize based on certain conditions
-        if (j === 1)  {
-            clearInterval(timeInterval);
-            timeLeft -= 15;
-            console.log('j is here 2:' + j)
-            countdown();
-        };
-        
-
-
-
     }, 1000);
 
     console.log(timeInterval)
     
 };
 
-// console.log(timeInterval)
+// let i = 0;
+// while (i<myQuestions.length) {
+//     askQuestion(i);
+// }
+
+
